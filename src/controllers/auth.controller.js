4d48adfc9aafcecc5a064473a2272ec25eff6633
meehhmett <49271158+mehweedd@users.noreply.github.com>
2026-01-02 01:40:50 +0300

    commit.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+const service = require("../services/auth.service");
+
+exports.register = async (req, res) => {
+    try {
+        const data = await service.register(req.body);
+        res.status(201).json(data);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const data = await service.login(req.body);
+        res.json(data);
+    } catch (err) {
+        res.status(401).json({ error: err.message });
+    }
+};
